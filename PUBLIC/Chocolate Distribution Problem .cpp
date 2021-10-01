042db68c2aa4a46868d290f519076eb2a72279d8
tomriddle77 <91708674+tomriddle77@users.noreply.github.com>
2021-10-01 18:09:27 +0530

    Create Chocolate Distribution Problem .cpp

---
+++ PUBLIC/Chocolate Distribution Problem .cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+        
+    //code
+        if (m == 0 || n == 0)
+            return 0;
+        if (n < m)
+            return -1;
+        sort(a.begin(),a.end());
+        
+        long long int ans=INT_MAX;
+        for(int i=0;i<n-m+1;i++)
+            ans=min(ans,a[i+m-1]-a[i]);
+        return ans;
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
